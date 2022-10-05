47918f5c4e9eefe9efacf88986b4572a5b0164a3
Roman Gurduz <gurduz@gmail.com>
2022-10-05 19:50:51 +0300

    added routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
+import { AppComponent } from './app.component';
+import { AboutComponent } from './components/pages/about/about.component';
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes} from '@angular/router'; 
+import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  //   { path: '', component: AppComponent },
+  { path: 'about', component: AboutComponent },
+];
 
 @NgModule({
-    imports: [RouterModule.forRoot(routes)],
-    exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
 })
-export class AppRoutingModule{}
+export class AppRoutingModule {}
